248edc8c5cba29a21380fce6aa5c05a2ca9c6f12
tooooofu24 <toya24xxx@i.softbank.jp>
2023-02-19 15:35:47 +0900

    linter, formatterの設定など

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { registerRootComponent } from "expo";
 
 export default function App() {
   return (
@@ -13,8 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+registerRootComponent(App); // 追加
